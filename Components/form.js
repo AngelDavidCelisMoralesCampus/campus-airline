78e888599d10511fa398330ef6ca5e13da135c38
AngelDavidCelisMoralesCampus <davidcelis1025@gmail.com>
2023-05-08 13:04:14 -0500

    Last All Commit

--- Components/form.js
+++ Components/form.js
@@ -39,7 +39,7 @@ export class MainForm extends HTMLElement{
                                 <div class="col-4">
                                     <label for="PrefHora" class="form-label">Preferencia Horaria</label>
                                     <select class="form-select" aria-label="Default select example">
-                                        <option selected style="text-muted">Seleccione el tipo</option>
+                                        <option selected style="">Seleccione el tipo</option>
                                         <option value="1">Primera Hora</option>
                                         <option value="2">Diurna</option>
                                         <option value="3">Medio Día</option>
--- Components/navbar.js
+++ Components/navbar.js
@@ -5,30 +5,62 @@ export class MainMenu extends HTMLElement{
     }
     render(){
         this.innerHTML=/* html */ `
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.html">Campus Airlines</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <span class="fs-5 d-none d-sm-inline">Campus Airline</span>
+                    </a>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                         <li class="nav-item">
-                            <a class="nav-link" href="tiquetes.html">Tiquetes</a>
+                            <a href="index.html" class="nav-link align-middle px-0">
+                                <i class='bx bx-home-alt'></i> <span class="ms-1 d-none d-sm-inline">Inicio</span>
+                            </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Rutas</a>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                <i class='bx bx-user-plus' ></i> <span class="ms-1 d-none d-sm-inline">Tiquetes</span></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="personal.html">Personal</a>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class='bx bx-search-alt-2' ></i> <span class="ms-1 d-none d-sm-inline">Rutas</span> </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Fidelización</a>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class='bx bx-search-alt-2' ></i> <span class="ms-1 d-none d-sm-inline">Personal</span> </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link px-0 align-middle">
+                                <i class='bx bx-search-alt-2' ></i> <span class="ms-1 d-none d-sm-inline">Fidelizacion</span> </a>
                         </li>
                     </ul>
                 </div>
             </div>
-        </nav>
+            <div class="col py-3">
+                <div class="container" id="lstClientes">
+                    <custom-list></custom-list>
+                </div>
+                <!-- Modal -->
+                <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Compra de Tiquetes</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <main-form></main-form>
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
         `
     }
 }
